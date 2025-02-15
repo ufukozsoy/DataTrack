5696f96258d6ff0558b964ab39fe62106456b5ea
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-02-15 19:17:42 +0100

    updated

---
+++ packages.py
@@ -0,0 +1,11 @@
+# Import pi function of math package
+from math import pi
+
+# Calculate C
+C = 2 * 0.43 * pi
+
+# Calculate A
+A = pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
