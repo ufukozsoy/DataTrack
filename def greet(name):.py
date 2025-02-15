382021bee7afeef215bf85b655a3f3b524ff9524
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-02-15 16:14:59 +0100

    updated

--- def greet(name):.py
+++ def greet(name):.py
@@ -1,6 +1,8 @@
-def greet(name):
-    return f"Hello, {name}!"
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 
-if __name__ == "__main__":
-    name = input("Enter your name: ")
-    print(greet(name))
+# Print out the index of the element 20.0
+
+print(areas.index(20.0))
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
