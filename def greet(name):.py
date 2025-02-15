4ec4e846abe834fd5d3d19b95e264bf17f3ff132
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-02-15 19:16:06 +0100

    updated

--- def greet(name):.py
+++ def greet(name):.py
@@ -22,3 +22,15 @@ print(areas)
 areas.reverse()
 # Print out areas
 print(areas)
+
+# Import the math package
+import math
+
+# Calculate C
+C = 2 * 0.43 * math.pi
+
+# Calculate A
+A = math.pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
