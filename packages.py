91cebbd9a07df99ca73499fa2e063b9c45c18b71
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-02-25 15:10:38 +0100

    updated

--- packages.py
+++ packages.py
@@ -21,3 +21,16 @@ np_baseball=np.array(baseball)
 
 # Print out type of np_baseball
 print(type(np_baseball))
+
+import numpy as np
+
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb=np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123, 0])
