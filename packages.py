d517eb1f770ba298cc883393bcaa407f0f7c6ce4
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-02-25 14:42:28 +0100

    updated

--- packages.py
+++ packages.py
@@ -9,3 +9,15 @@ A = pi * 0.43 ** 2
 
 print("Circumference: " + str(C))
 print("Area: " + str(A))
+
+# Import the numpy package as np
+import numpy as np
+
+
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Create a numpy array from baseball: np_baseball
+np_baseball=np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
