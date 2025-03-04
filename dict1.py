b104b74c480ed8c80a8f283665c8fdd2727060c0
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-03-04 16:26:10 +0100

    updated

--- dict1.py
+++ dict1.py
@@ -7,3 +7,13 @@ ind_ger=countries.index("germany")
 
 # Use ind_ger to print out capital of Germany
 print(capitals[ind_ger])
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = {'spain':'madrid', "france":"paris","germany":"berlin","norway":"oslo" }
+
+# Print europe
+print(europe)
