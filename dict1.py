465ef641f9d6e287b0c1864b8deb2f75b9d315eb
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-03-04 16:40:37 +0100

    updated

--- dict1.py
+++ dict1.py
@@ -42,3 +42,19 @@ europe["poland"]="warsaw"
 
 # Print europe
 print(europe)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe["germany"]="berlin"
+
+# Remove australia
+del(europe["australia"])
+
+
+# Print europe
+print(europe)
+
