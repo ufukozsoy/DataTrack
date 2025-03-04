80394738afaf469edb05f42b61b142a7ed10c455
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-03-04 16:28:26 +0100

    updated

--- dict1.py
+++ dict1.py
@@ -17,3 +17,13 @@ europe = {'spain':'madrid', "france":"paris","germany":"berlin","norway":"oslo" 
 
 # Print europe
 print(europe)
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe["norway"])
