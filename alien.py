7f89e5612e28287080eb0c22fb7a08000e069bae
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-05 16:39:09 +0200

    updated

--- alien.py
+++ alien.py
@@ -176,3 +176,12 @@ user_0 = {
     'first': 'enrico',
     'last': 'fermi',
 }
+# Store additional information about the user
+user_0['location'] = 'chicago'
+user_0['age'] = 39
+# Display the user's information
+print(f"Username: {user_0['username']}")
+print(f"Full name: {user_0['first']} {user_0['last']}")
+print(f"Location: {user_0['location']}")
+print(f"Age: {user_0['age']}")
+# A dictionary of similar object
