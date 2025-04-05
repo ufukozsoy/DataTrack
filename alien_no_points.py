7617048b41a521570f60657c80eab4c60252e6c2
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-05 22:47:44 +0200

    updated

--- alien_no_points.py
+++ alien_no_points.py
@@ -3,3 +3,14 @@ alien={"color":"green","speed":"medium"}
 point_value=alien.get("points","No point value assigned.")
 print(point_value)
 
+
+# # Show the color of the third alien
+# print(aliens[2]['color'])
+# # Show the points of the first alien
+# print(aliens[0]['points'])
+# # Show the points of the second alien
+# print(aliens[1]['points'])
+# # Show the points of the third alien
+# print(aliens[2]['points'])
+# # Show the speed of the first alien
+# print(aliens[0]['speed'])
