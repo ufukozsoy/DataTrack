b95827ec4ef6f70239916be91348488b24111147
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-09 15:12:11 +0200

    updated

--- magic_number.py
+++ magic_number.py
@@ -1,5 +1,15 @@
 number=17
 if number!=47:
     print("The number isnt correct, please try again")
+else:
+    print("The number is correct")
+#
+# # Output:
+# # The number isnt correct, please try again
+# # Output:
+# # The number is correct
+#
+# # Output:
+# # The number isnt correct, please try again
 
 
