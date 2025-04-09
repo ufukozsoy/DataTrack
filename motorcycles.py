444211858a75e0cf9235a757d0ff51aba12d887b
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-09 15:13:31 +0200

    updated

--- motorcycles.py
+++ motorcycles.py
@@ -2,4 +2,8 @@ motorcycles=["honda","mercedes","samsung"]
 print(motorcycles)
 motorcycles[0]="ducati"
 print(motorcycles)
+motorcycles.append("suzuki")
+print(motorcycles)
+motorcycles.insert(0,"yamaha")
+print(motorcycles)
 
