b409ad6a0a63475e898c10c34642f55aa3f43098
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-09 15:13:12 +0200

    updated

--- magicians.py
+++ magicians.py
@@ -8,3 +8,6 @@ for magician in magicians:
 #	print(f"{magician.title()}, that was awesome. ")
 #	print(f"{magician.title()}, we are waiting for your next trick.\n")
 #print("Thank you everyone.")
+#for magician in magicians:
+#	print(f"{magician.title()}, that was awesome. ")
+#	print(f"{magician.title()}, we are waiting for your next trick.\n")
