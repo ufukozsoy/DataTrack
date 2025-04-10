3db167794c69fb85f698119d889f75cf3500eae7
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-10 21:03:05 +0200

    updated

--- personal_message.py
+++ personal_message.py
@@ -1,3 +1,7 @@
 name="Eric"
 personal_message=f"Hello {name}, would you like to study with me?"
 print(personal_message)
+# Output:
+# Hello Eric, would you like to study with me?
+# Output:
+# Hello Eric, would you like to study with me?
