45a7d3a4065656f470da7933d7dcdd4be55e9137
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-10 21:22:14 +0200

    updated

--- example_4.3.py
+++ example_4.3.py
@@ -14,4 +14,10 @@ numbers=list(range(1,1000001))
 print(min(numbers))
 print(max(numbers))
 print(sum(numbers))
+#squares
+squares=[value**2 for value in range(1,11)]
+print(squares)
+#squares
+squares=[value**2 for value in range(1,11)]
+print(squares)
 
