44c577a28f29b798092bdbfa3ab2c162eeeae26b
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-13 23:51:35 +0200

    updated

--- famous_code.py
+++ famous_code.py
@@ -1,3 +1,6 @@
 author="Albert einstein"
 quote=  ' "a person who never made a mistake never tried anything new" '
 print(author + " once said," + quote )
+# Output:
+# Albert einstein once said, "a person who never made a mistake never tried anything new"
+# Output:
