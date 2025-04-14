ca4a1e8c7347ef24abcab6d035d0b0092630d113
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-04-15 00:17:43 +0200

    updated

--- yourownlist.py
+++ yourownlist.py
@@ -1,3 +1,7 @@
 cars=["audi","vw","mercedes","bmw"]
 message=f"I would like to have {cars[1].title()} in the future."
 print(message)
+message=f"I would like to have {cars[2].title()} in the future."
+print(message)
+message=f"I would like to have {cars[3].title()} in the future."
+print(message)
