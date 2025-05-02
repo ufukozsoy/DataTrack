1e82aef3a7a5e58fc46bcd202521d8c9abd47108
ufukozsoy <ufuk.ozsoy@powercoders.org>
2025-05-02 16:13:18 +0200

    updated

--- agame7.py
+++ agame7.py
@@ -15,7 +15,7 @@ BLACK = (0, 0, 0)
 
 # Paddle dimensions
 PADDLE_WIDTH, PADDLE_HEIGHT = 10, 100
-BALL_RADIUS = 10
+BALL_RADIUS = 11
 
 # Paddle positions
 player1_x, player1_y = 50, HEIGHT // 2 - PADDLE_HEIGHT // 2
